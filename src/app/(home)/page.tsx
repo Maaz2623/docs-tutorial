4d74ee563cd21e61385d4fb8178f7e870df44a16
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-12-04 00:21:19 +0530

    new file:   src/app/(home)/navbar.tsx 	new file:   src/app/(home)/page.tsx 	new file:   src/app/(home)/search-input.tsx 	deleted:    src/app/page.tsx

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+import { Navbar } from "./navbar";
+
+const HomePage = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
+        <Navbar />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
