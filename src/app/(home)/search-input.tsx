b133a26b9113023d3107c05c843606c8463fdd89
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-12-04 14:51:16 +0530

    modified:   bun.lockb 	modified:   package.json 	modified:   src/app/(home)/search-input.tsx 	modified:   src/app/layout.tsx 	new file:   src/hooks/use-search-param.ts

--- src/app/(home)/search-input.tsx
+++ src/app/(home)/search-input.tsx
@@ -1,19 +1,39 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { SearchIcon } from "lucide-react";
+import { useSearchParam } from "@/hooks/use-search-param";
+import { SearchIcon, XIcon } from "lucide-react";
 import { useRef, useState } from "react";
 
 export const SearchInput = () => {
-  const [value, setVaule] = useState();
+  const [search, setSearch] = useSearchParam("search");
+  const [value, setVaule] = useState(search);
 
   const inputRef = useRef<HTMLInputElement>(null);
 
-  
+  const handelChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setVaule(e.target.value);
+  };
+
+  const handleClear = () => {
+    setVaule("");
+    setSearch("");
+    inputRef.current?.blur();
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch(value);
+    inputRef.current?.blur();
+  };
 
   return (
     <div className="flex-1 flex items-center justify-center">
-      <form className="relative max-w-[720px] w-full">
+      <form onSubmit={handleSubmit} className="relative max-w-[720px] w-full">
         <Input
+          value={value}
+          onChange={handelChange}
+          ref={inputRef}
           placeholder="Search"
           className="md:text-base placeholder:text-neutral-800 px-14 w-full border-none bg-[#f0f4f8] rounded-full h-[48px] focus-visible:ring-0 focus:bg-white focus-visible:shadow-[0_1px_1px_0_rgba(65, 69, 73, .3), 0_1px_3px_1px_rgba(65,69,73,.15)]"
         />
@@ -25,6 +45,17 @@ export const SearchInput = () => {
         >
           <SearchIcon />
         </Button>
+        {value && (
+          <Button
+            onClick={handleClear}
+            type="button"
+            variant={`ghost`}
+            size="icon"
+            className="absolute right-3 top-1/2 -translate-y-1/2 [&_svg]:size-5 rounded-full"
+          >
+            <XIcon />
+          </Button>
+        )}
       </form>
     </div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <NuqsAdapter>{children}</NuqsAdapter>
+      </body>
     </html>
   );
 }
