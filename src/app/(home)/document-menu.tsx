605316e6dfb8d271e2a78b2e1b6f3d4afa13bd10
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-12-04 22:20:42 +0530

    modified:   convex/documents.ts 	new file:   src/app/(home)/document-menu.tsx 	modified:   src/app/(home)/document-row.tsx 	modified:   src/app/(home)/documents-table.tsx 	new file:   src/components/remove-dialog.tsx 	new file:   src/components/rename-dialog.tsx

---
+++ src/app/(home)/document-menu.tsx
@@ -0,0 +1,59 @@
+import {
+  ExternalLinkIcon,
+  FilePenIcon,
+  MoreVerticalIcon,
+  TrashIcon,
+} from "lucide-react";
+import { Id } from "../../../convex/_generated/dataModel";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { RemoveDialog } from "@/components/remove-dialog";
+import { RenameDialog } from "@/components/rename-dialog";
+
+interface DocumentMenuProps {
+  documentId: Id<"documents">;
+  title: string;
+  onNewTab: (id: Id<"documents">) => void;
+}
+
+export const DocumentMenu = ({
+  documentId,
+  title,
+  onNewTab,
+}: DocumentMenuProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="focus:outline-none focus-visible:ring-0 focus:ring-0">
+        <MoreVerticalIcon className="size-4" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <RemoveDialog documentId={documentId}>
+          <DropdownMenuItem
+            onSelect={(e) => e.preventDefault()}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <TrashIcon className="size-4 mr-2" />
+            Remove
+          </DropdownMenuItem>
+        </RemoveDialog>
+        <RenameDialog initialTitle={title} documentId={documentId}>
+          <DropdownMenuItem
+            onSelect={(e) => e.preventDefault()}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <FilePenIcon className="size-4 mr-2" />
+            Rename
+          </DropdownMenuItem>
+        </RenameDialog>
+        <DropdownMenuItem onClick={() => onNewTab(documentId)}>
+          <ExternalLinkIcon className="size-4 mr-2" />
+          Open in a new tab
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
