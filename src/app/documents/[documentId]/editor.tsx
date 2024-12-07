a844bdffb803f0c3a67286186bbeef29985067d3
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-12-07 20:08:00 +0530

    modified:   src/app/documents/[documentId]/editor.tsx 	modified:   src/app/documents/[documentId]/room.tsx 	modified:   src/app/documents/[documentId]/ruler.tsx 	modified:   src/app/documents/[documentId]/threads.tsx 	new file:   src/constants/margins.ts

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -26,11 +26,14 @@ import { useStorage } from "@liveblocks/react/suspense";
 
 import { useLiveblocksExtension } from "@liveblocks/react-tiptap";
 import { Threads } from "./threads";
+import { LEFT_MARGIN_DEFAULT, RIGHT_MARGIN_DEFAULT } from "@/constants/margins";
 
 export const Editor = () => {
   const leftMargin = useStorage((root) => root.leftMargin);
   const rightMargin = useStorage((root) => root.rightMargin);
-  const liveblocks = useLiveblocksExtension();
+  const liveblocks = useLiveblocksExtension({
+    offlineSupport_experimental: true,
+  });
   const { setEditor } = useEditorStore();
 
   const editor = useEditor({
@@ -61,7 +64,7 @@ export const Editor = () => {
     },
     editorProps: {
       attributes: {
-        style: `padding-left: ${leftMargin ?? 56}px; padding-right: ${rightMargin ?? 56}px;`,
+        style: `padding-left: ${leftMargin ?? LEFT_MARGIN_DEFAULT}px; padding-right: ${rightMargin ?? RIGHT_MARGIN_DEFAULT}px;`,
         class:
           "focus:outline-none rounded-md print:border-0 bg-white border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text border",
       },
--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -11,6 +11,7 @@ import { FullscreenLoader } from "@/components/fullscreen-loader";
 import { getDocuments, getUsers } from "./action";
 import toast from "react-hot-toast";
 import { Id } from "../../../../convex/_generated/dataModel";
+import { LEFT_MARGIN_DEFAULT, RIGHT_MARGIN_DEFAULT } from "@/constants/margins";
 
 type User = {
   id: string;
@@ -79,8 +80,8 @@ export function Room({ children }: { children: ReactNode }) {
       <RoomProvider
         id={params.documentId as string}
         initialStorage={{
-          leftMargin: 56,
-          rightMargin: 56,
+          leftMargin: LEFT_MARGIN_DEFAULT,
+          rightMargin: RIGHT_MARGIN_DEFAULT,
         }}
       >
         <ClientSideSuspense
